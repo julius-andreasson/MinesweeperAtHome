097c58040e5c25c7a196d430acea1beacc4dba76
julius-andreasson <juliusnandreasson@gmail.com>
2022-10-11 20:40:11 +0200

    Moved RNG to Util class.

---
+++ src/Utils/Util.java
@@ -0,0 +1,13 @@
+package Utils;
+
+public class Util {
+    /**
+	 * Returns a random integer min <= int <= max.
+	 * @param min
+	 * @param max
+	 * @return
+	 */
+	public static int randomNumber(int min, int max) {
+		return (int)Math.round((Math.random() * (max - min) + min));
+	}
+}
