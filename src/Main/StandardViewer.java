6baab47fad29969d1c6b2bce85f7a4254af1a773
julius-andreasson <juliusnandreasson@gmail.com>
2022-10-11 20:42:17 +0200

    StandardViewer changed to know nothing about Map

--- src/Main/StandardViewer.java
+++ src/Main/StandardViewer.java
@@ -4,11 +4,12 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import Utils.Point;
+import Utils.Settings;
 
 import javax.swing.JPanel;
 
 public class StandardViewer extends JPanel {
-	Game game;
+	private Game game;
 
 	public StandardViewer (Game game) {
 		this.game = game;
@@ -22,38 +23,38 @@ public class StandardViewer extends JPanel {
 		g.setFont(new Font(Font.MONOSPACED, Font.PLAIN, Settings.tileSizeX));
 		
 		//Draw Tiles
-		for (int x = 0; x < game.map.tileMap.length; x++) {
-			for (int y = 0; y < game.map.tileMap[0].length; y++) {
-				paintTile(g, x, y);
+		for (int x = 0; x < Settings.tileCountX; x++) {
+			for (int y = 0; y < Settings.tileCountY; y++) {
+				paintTile(g, new Point(x, y));
 			}
 		}
 		
 		//Draw text; game-over if (end_Loss)
 		g.setFont(new Font(Font.MONOSPACED, Font.PLAIN, Settings.tileSizeY - 3));
 		g.setColor(Color.black);
-		if (game.end_Loss) {
+		if (game.state == State.LOST) {
 			g.drawString("Game over! Press 'R' to restart.", Settings.borderSizeX - 5, Settings.borderSizeY + 20);
-			g.drawString("You cleared " + (game.map.tilesDugUp) + " out of " + (game.tilesToWin) + " tiles.", Settings.borderSizeX - 5, Settings.borderSizeY + 25 + Settings.tileSizeY);
-		} else if (game.end_Win){
+			g.drawString("You cleared " + (game.getTilesCleared()) + " out of " + (Settings.tilesToWin) + " tiles.", Settings.borderSizeX - 5, Settings.borderSizeY + 25 + Settings.tileSizeY);
+		} else if (game.state == State.WON){
 			g.drawString("You've won! Press 'R' to restart.", Settings.borderSizeX, Settings.borderSizeY + 20);
-			g.drawString("You cleared " + (game.map.tilesDugUp) + " out of " + (game.tilesToWin) + " tiles.", Settings.borderSizeX - 5, Settings.borderSizeY + 25 + Settings.tileSizeY);
+			g.drawString("You cleared " + (game.getTilesCleared()) + " out of " + (Settings.tilesToWin) + " tiles.", Settings.borderSizeX - 5, Settings.borderSizeY + 25 + Settings.tileSizeY);
 		} else {
 			//TopUI
-			g.drawString("Remaining flags: " + game.map.remainingFlags, Settings.borderSizeX, Settings.borderSizeY + 20);
-			g.drawString("Tiles cleared: " + game.map.tilesDugUp, Settings.borderSizeX, Settings.borderSizeY + 50);
+			g.drawString("Remaining flags: " + game.getRemainingFlags(), Settings.borderSizeX, Settings.borderSizeY + 20);
+			g.drawString("Tiles cleared: " + game.getTilesCleared(), Settings.borderSizeX, Settings.borderSizeY + 50);
 		}
 	}
 	
-	public void paintTile(Graphics g, int tileX, int tileY) {
+	public void paintTile(Graphics g, Point p) {
 		Color tileColor, stringColor; 
-		Point pos = Settings.pixelsFromTile(tileX, tileY);
+		Point pos = Settings.pixelsFromTile(p.x, p.y);
 		
-		if (game.map.tileMap[tileX][tileY].isChecked) {
+		if (game.isChecked(p)) {
 			tileColor = Color.lightGray; 
 		}
 		else
 		{
-			if(game.map.tileMap[tileX][tileY].isFlagged) {
+			if(game.isFlagged(p)) {
 				tileColor = Color.red; 
 			}
 			else {
@@ -63,18 +64,18 @@ public class StandardViewer extends JPanel {
 		g.setColor(tileColor);
 		g.fillRect(pos.x, pos.y, Settings.tileSizeX, Settings.tileSizeY);
 		//If the player has lost the game, draw a mine on the current tile if it 'hasMine'. 
-		if ((game.end_Loss || game.end_Win) && game.map.tileMap[tileX][tileY].hasMine) {
+		if (game.state != State.ONGOING && game.hasMine(p)) {
 			//Set the color of the mines. 
 			g.setColor(Color.black);
 			//Draw an oval that represents a mine. 
 			g.fillOval(pos.x, pos.y, Settings.tileSizeX, Settings.tileSizeY);
 		}
 		// Draw the number of mines on each checked tile. 
-		if (!game.map.tileMap[tileX][tileY].hasMine) {
-			if(game.map.tileMap[tileX][tileY].isChecked && game.map.tileMap[tileX][tileY].surroundingMines > 0) {
+		if (!game.hasMine(p)) {
+			if(game.isChecked(p) && game.getSurroundingMines(p) > 0) {
 				stringColor = Color.black; 
 				g.setColor(stringColor); 
-				g.drawString("" + game.map.tileMap[tileX][tileY].surroundingMines, pos.x + Settings.tileSpacingX, pos.y + Settings.tileSizeY - Settings.tileSpacingY);
+				g.drawString("" + game.getSurroundingMines(p), pos.x + Settings.tileSpacingX, pos.y + Settings.tileSizeY - Settings.tileSpacingY);
 			}
 		}
 	}
